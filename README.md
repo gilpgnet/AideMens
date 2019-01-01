e4abfff852c59db56ec77126323d36b0b90ebc40
gilpgnet <gilpgnet@gmail.com>
2019-01-01 14:15:36 -0600

    Comentarios explicando código

---
+++ README.md
@@ -0,0 +1,2 @@
+# AideAmens
+App sencilla para AIDE IDE que muestra mensajes.
--- app/src/main/java/com/amens/MainActivity.java
+++ app/src/main/java/com/amens/MainActivity.java
@@ -1,14 +1,44 @@
+//Paquete de la aplicación.
 package com.amens;
 
+/* Indica que cuando es necesite una clase, se busque en el paquete
+ * "android.app". En este caso encuantra la clase Activity.*/
 import android.app.*;
+/* Indica que cuando es necesite una clase, se busque en el paquete
+ * "android.os". En este caso encuantra la clase Bundle.*/
 import android.os.*;
 
+/** La clase MainActivity, se puede usar desde cualquier paquete de la
+ * aplicación (porque es public) y hereda de la clase Activity. */
 public class MainActivity extends Activity 
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+  /* @Override que anota
+   * protected void onCreate(Bundle savedInstanceState), indica que se
+   * modifica la definición que la superclase AppCompatActivity tiene de
+   * este método. Esto también significa que el método debe definirse en
+   * la clase madre Activity. */
+  @Override
+  /* protected void onCreate(Bundle savedInstanceState) define un método
+   * que corresponde al evento create. Cuando se lanza la aplicación o se
+   * rota el dispositivo que ejecuta esta App, se crea una instancia de
+   * esta clase y se invoca este método para iniciar su funcionamiento. Lo
+   * mínimo que debe hacer es asociar una vista. */
+  protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    /* Invoca la versión de este evento que está definida en la clase
+     * madre AppCompatActivity. Muchos de los métodos que se sobreescriben
+     * en Android, deben invocar la versión de la superclase. */
+    super.onCreate(savedInstanceState);
+    /* Asigna la vista de esta activity. La clase R se genera
+     * automáticamente en el paquete de la aplicación a partir del
+     * contenido de la carpeta res. Para cada subcarpeta, por ejemplo
+     * layout, se genera una clase interna; que en este caso, se llama
+     * layout. Por cada archivo, se genera una constante entera, que en
+     * este caso se llama main. Si alguno de los archivos res
+     * tiene algún error, la clase R no se genera. Esta clase nos ayuda a
+     * introducir el nombre correcto para cada archivo. El proceso con el
+     * cual se construye la interfaz gráfica a partir de un archivo xml de
+     * Android se conoce como inflate (inflar). */
+    setContentView(R.layout.main);
     }
 }
